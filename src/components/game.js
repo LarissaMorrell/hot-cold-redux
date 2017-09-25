62d40497bf8d6bd206beba44b7668b9f6ff4fe83
LarissaMorrell <larissa@larissamorrell.com>
2017-09-25 14:15:21 -0400

    started changes for redux and makeGuess action

--- src/components/game.js
+++ src/components/game.js
@@ -1,17 +1,18 @@
 import React from 'react';
-
 import Header from './header';
 import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
+import {connect} from 'react-redux';
+import {makeGuess} from '../actions';
 
-export default class Game extends React.Component {
+export class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             guesses: [],
             feedback: 'Make your guess!',
-            correctAnswer: Math.floor(Math.random() * 100) + 1,
+            correctAnswer: Math.floor(Math.random() * 100) + 1
         };
     }
 
@@ -23,38 +24,8 @@ export default class Game extends React.Component {
         });
     }
 
-    guess(guess) {
-        guess = parseInt(guess, 10);
-        if (isNaN(guess)) {
-            this.setState({
-                feedback: 'Please enter a valid number'
-            });
-            return;
-        }
-
-        const difference = Math.abs(guess - this.state.correctAnswer);
-
-        let feedback;
-        if (difference >= 50) {
-            feedback = 'You\'re Ice Cold...';
-        }
-        else if (difference >= 30) {
-            feedback = 'You\'re Cold...';
-        }
-        else if (difference >= 10) {
-            feedback = 'You\'re Warm';
-        }
-        else if (difference >= 1) {
-            feedback = 'You\'re Hot!';
-        }
-        else {
-            feedback = 'You got it!';
-        }
-
-        this.setState({
-            feedback,
-            guesses: [...this.state.guesses, guess]
-        });
+    makeGuess(guess) {
+        this.props.dispatch(makeGuess(guess));
     }
 
     render() {
@@ -62,7 +33,7 @@ export default class Game extends React.Component {
             <div>
                 <Header onNewGame={() => this.newGame()}/>
                 <GuessSection feedback={this.state.feedback}
-                    onGuess={(guess) => this.guess(guess)} />
+                    onGuess={(guess) => this.makeGuess(guess)} />
                 <GuessCount count={this.state.guesses.length} />
                 <GuessList guesses={this.state.guesses} />
             </div>
@@ -70,3 +41,14 @@ export default class Game extends React.Component {
     }
 }
 
+// Game.defaultProps = {
+//     title: 'Board'
+// };
+
+const mapStateToProps = state => ({
+    guesses: state.guesses,
+    feedback: state.feedback,
+    correctAnswer: state.correctAnswer
+});
+
+export default connect(mapStateToProps)(Game);
---
+++ src/reducers/index.js
@@ -0,0 +1,43 @@
+import * as actions from '../actions';
+
+const initialState = {
+    guesses: [],
+    feedback: 'Make your guess!',
+    correctAnswer: Math.floor(Math.random() * 100) + 1
+};
+
+export const hotColdReducer = (state=initialState, action) => {
+    if (action.type === actions.MAKE_GUESS) {
+        const guess = parseInt(action.guess, 10);
+
+        if (isNaN(guess)) {
+            return Object.assign({}, state, {
+              feedback: 'Please enter a valid number'});
+        }
+
+        const difference = Math.abs(guess - state.correctAnswer);
+        let feedback;
+
+        if (difference >= 50) {
+            feedback = 'You\'re Ice Cold...';
+        }
+        else if (difference >= 30) {
+            feedback = 'You\'re Cold...';
+        }
+        else if (difference >= 10) {
+            feedback = 'You\'re Warm';
+        }
+        else if (difference >= 1) {
+            feedback = 'You\'re Hot!';
+        }
+        else {
+            feedback = 'You got it!';
+        }
+
+        return Object.assign({}, state, { //copy the state object
+            guesses: [...state.guesses, guess],
+            feedback: feedback
+        });
+    }
+    return state;
+};
